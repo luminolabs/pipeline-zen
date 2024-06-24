5878d1a60aea8ac96a652d09add25558faf1453c
Vasilis Raptakis <scaraveos@gmail.com>
2024-06-23 20:03:26 -0600

    WIP fix multi device torchtune

--- scripts/mig-startup-script.sh
+++ scripts/mig-startup-script.sh
@@ -13,17 +13,11 @@ export $(grep -v '^#' ./.env | xargs)
 # Define the log file path
 LOG_FILE="./output.log"
 
-# Call the pubsub-job-runner.sh script and redirect stdout and stderr to the log file
-./scripts/pubsub-job-runner.sh > "$LOG_FILE" 2>&1
+# Redirect both stdout and stderr to the log file and to stdout/stderr using tee
+exec > >(tee -a "$LOG_FILE") 2>&1
 
-# Delete the VM after the script finishes;
-# also removes the VM from the MIG
-python ./scripts/delete_vm.py >> "$LOG_FILE" 2>&1
+# Call the pubsub-job-runner.sh script
+./scripts/pubsub-job-runner.sh
 
-# Copy output.log to the only folder under .logs
-LOGS_DIR=$(find .logs -mindepth 1 -maxdepth 1 -type d | head -n 1)
-if [ -d "$LOGS_DIR" ]; then
-  cp "$LOG_FILE" "$LOGS_DIR/"
-else
-  echo "No directory found under .logs"
-fi
+# Delete the VM after the script finishes; also removes the VM from the MIG
+python ./scripts/delete_vm.py
