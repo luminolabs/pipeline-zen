e49fc8556843cae442c9634fbb39417c51dc7921
Shyam Patel <shyampkira@gmail.com>
2024-06-26 10:52:08 -0700

    update config for Mistral-7b v0.1 with lora and full

--- lib-workflows/torchtunewrapper/src/torchtunewrapper/utils.py
+++ lib-workflows/torchtunewrapper/src/torchtunewrapper/utils.py
@@ -26,6 +26,7 @@ def get_torchtune_config_filename(model_base: str, use_lora: bool, use_single_de
         'meta-llama/Meta-Llama-3-8B': 'llama3/8B',
         'meta-llama/Meta-Llama-3-70B': 'llama3/70B',
         'mistralai/Mistral-7B-v0.3': 'mistral/7B',
+        'mistralai/Mistral-7B-v0.1': 'mistral/7B',
     }
     # Raise error if model base is not supported
     if model_base not in model_base_to_config_prefix:
