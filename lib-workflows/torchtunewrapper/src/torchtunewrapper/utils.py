ec3bbdce435ed5165df4f6f4294486d5f9260bd0
Shyam Patel <shyampkira@gmail.com>
2024-06-26 16:23:28 -0700

    add instruct models

--- lib-workflows/torchtunewrapper/src/torchtunewrapper/utils.py
+++ lib-workflows/torchtunewrapper/src/torchtunewrapper/utils.py
@@ -27,6 +27,7 @@ def get_torchtune_config_filename(model_base: str, use_lora: bool, use_single_de
         'meta-llama/Meta-Llama-3-70B': 'llama3/70B',
         'mistralai/Mistral-7B-Instruct-v0.1': 'mistral/7B',
         'mistralai/Mistral-7B-Instruct-v0.3': 'mistral/7B',
+        'mistralai/Mixtral-8x7B-Instruct-v0.1': 'mistral/8x7B',
         'mistralai/Mistral-7B-v0.3': 'mistral/7B',
         'mistralai/Mistral-7B-v0.1': 'mistral/7B',
     }
