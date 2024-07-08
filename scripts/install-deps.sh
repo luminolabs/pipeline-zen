ade6e0e01cdbaf98db91b8903cf814b385846248
Vasilis Raptakis <scaraveos@gmail.com>
2024-07-08 10:39:13 -0700

    LUM-188: Fixed creating results folder

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -3,6 +3,10 @@
 # This script should be run locally only. It will install all dependencies required for the ML pipeline.
 # Remote environments should use the Docker image to run the pipeline
 
+# Upgrade pip
+pip install -U pip
+
+# Install the required packages
 pip install -Ur lib-common/requirements.txt
 pip install -Ur lib-common/requirements-test.txt
 pip install -Ur lib-workflows/train/requirements.txt
