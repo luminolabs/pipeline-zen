efa1aac18c0c5650522996c9b0763d3ccb25d83b
Vasilis Raptakis <scaraveos@gmail.com>
2024-07-24 19:42:05 -0700

    LUM-448: Fix logging issue

--- lib-workflows/torchtunewrapper/src/torchtunewrapper/workflow.py
+++ lib-workflows/torchtunewrapper/src/torchtunewrapper/workflow.py
@@ -39,7 +39,7 @@ def run(job_config: DictConfig, tt_config: DictConfig, logger: Logger) -> dict:
     # Log a few things about this job
     scores_logger.info('The job id is: ' + job_id)
     scores_agent.log_system_specs()
-    scores_agent.log_job_config(tt_config)
+    scores_agent.log_job_config(job_config)
 
     # Instantiate dataset
     dataset = chat_dataset(
