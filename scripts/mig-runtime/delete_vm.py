bfa6b05ecc2379b7f22cab467b72def66279081a
Vasilis Raptakis <scaraveos@gmail.com>
2024-09-05 11:41:49 -0700

    LUM-669: Supporting changes for scheduler (detach VMs from MIG on `VM_FOUND` status) (#88)

--- scripts/mig-runtime/delete_vm.py
+++ scripts/mig-runtime/delete_vm.py
@@ -6,8 +6,7 @@ import os
 
 from google.cloud import compute_v1
 
-from common.gcp import get_mig_name_from_vm_name, get_vm_name_from_metadata, get_zone_from_metadata, \
-    get_region_from_zone
+from common.gcp import get_vm_name_from_metadata, get_zone_from_metadata
 from utils import PROJECT_ID
 
 # Configure logging
@@ -15,39 +14,37 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 logger = logging.getLogger(__name__)
 
 
-def delete_instance_from_mig(project_id: str, vm_zone: str, mig_name: str, vm_name: str) -> None:
+def delete_instance(project_id: str, zone: str, instance_name: str) -> None:
     """
-    Delete an instance from a regional Managed Instance Group (MIG).
+    Delete a VM instance that's not part of a Managed Instance Group.
 
     Args:
         project_id (str): The Google Cloud project ID.
-        vm_zone (str): The zone of the VM instance.
-        mig_name (str): The name of the Managed Instance Group.
-        vm_name (str): The name of the VM instance to delete.
+        zone (str): The zone of the VM instance.
+        instance_name (str): The name of the VM instance to delete.
     """
-    # Get the region from the zone
-    vm_region = get_region_from_zone(vm_zone)
+    # Initialize the Compute Engine client
+    instances_client = compute_v1.InstancesClient()
 
-    # Initialize the regional MIG VM service
-    instance_group_managers_client = compute_v1.RegionInstanceGroupManagersClient()
-
-    # Create the VM delete request
-    request = compute_v1.DeleteInstancesRegionInstanceGroupManagerRequest(
-        instance_group_manager=mig_name,
-        region_instance_group_managers_delete_instances_request_resource=
-        compute_v1.RegionInstanceGroupManagersDeleteInstancesRequest(
-            instances=[f'zones/{vm_zone}/instances/{vm_name}']
-        ),
+    # Create the delete request
+    request = compute_v1.DeleteInstanceRequest(
         project=project_id,
-        region=vm_region,
+        zone=zone,
+        instance=instance_name
     )
 
-    # Delete VM from MIG
-    logger.info(f"Deleting VM {vm_name} from MIG {mig_name} in zone {vm_zone}")
-    operation = instance_group_managers_client.delete_instances(request=request)
+    # Delete the VM instance
+    print(f"Deleting VM {instance_name} in zone {zone}")
+    operation = instances_client.delete(request=request)
+
     # Wait for the operation to complete
-    operation.result()
-    logger.info(f"VM {vm_name} deleted successfully")
+    while not operation.done():
+        operation.result()
+
+    if operation.error:
+        print(f"Error deleting VM: {operation.error}")
+    else:
+        print(f"VM {instance_name} deleted successfully")
 
 
 if __name__ == '__main__':
@@ -58,15 +55,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Delete a VM instance from a Managed Instance Group.')
     parser.add_argument('--vm_name', type=str, help='Name of the VM instance', required=args_required)
     parser.add_argument('--vm_zone', type=str, help='Zone of the VM instance', required=args_required)
-    parser.add_argument('--mig_name', type=str, help='MIG of the VM instance', required=args_required)
     args = parser.parse_args()
 
     # Get the values from the metadata server if not provided as arguments
     vm_name = args.vm_name if args.vm_name else get_vm_name_from_metadata()
     vm_zone = args.vm_zone if args.vm_zone else get_zone_from_metadata()
-    mig_name = args.mig_name if args.mig_name else get_mig_name_from_vm_name(vm_name)
 
-    logger.info(f'Initiating deletion of VM {vm_name} from MIG {mig_name} in zone {vm_zone}')
+    logger.info(f'Initiating deletion of VM {vm_name} in zone {vm_zone}')
     # Delete the VM from the MIG
-    delete_instance_from_mig(PROJECT_ID, vm_zone, mig_name, vm_name)
+    delete_instance(PROJECT_ID, vm_zone, vm_name)
     logger.info('VM deletion process completed')
