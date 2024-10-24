fcec3b7752bda2e3b89dda0f94f71428f6195537
Vasilis Raptakis <scaraveos@gmail.com>
2024-10-24 13:38:40 -0700

    Read .env from runner scripts

--- scripts/runners/celery-wf.sh
+++ scripts/runners/celery-wf.sh
@@ -10,5 +10,14 @@ paths=$paths:$(pwd)/lib-workflows/train/src:$(pwd)/lib-workflows/evaluate/src
 paths=$paths:$(pwd)/lib-workflows/torchtunewrapper/src
 paths=$paths:$(pwd)/lib-celery/src
 
+# Export .env environment variables
+eval $(cat ./.env | grep -v '^#' | tr -d '\r')
+echo "PZ_ENV set to $PZ_ENV"
+
+# Export the variables so they're available to the python script
+export PZ_ENV
+export PZ_HUGGINGFACE_TOKEN
+export PZ_CUSTOMER_API_KEY
+
 # Run the Celery pipeline for the specified workflow
 PYTHONPATH=$paths python lib-celery/src/pipeline/$1_wf.py "${@:2}"
--- scripts/runners/single-wf.sh
+++ scripts/runners/single-wf.sh
@@ -7,5 +7,14 @@ set -e  # Exit immediately if a command fails
 # Set the paths to the Python modules
 paths=$(pwd)/lib-common/src:$(pwd)/lib-workflows/$1/src
 
+# Export .env environment variables
+eval $(cat ./.env | grep -v '^#' | tr -d '\r')
+echo "PZ_ENV set to $PZ_ENV"
+
+# Export the variables so they're available to the python script
+export PZ_ENV
+export PZ_HUGGINGFACE_TOKEN
+export PZ_CUSTOMER_API_KEY
+
 # Run the script for the specified workflow
 PYTHONPATH=$paths python lib-workflows/$1/src/$1/cli.py "${@:2}"
