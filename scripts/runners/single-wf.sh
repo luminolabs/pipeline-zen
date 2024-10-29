c56fca16fb86fb08755b6d05e0a1b9f10e0169ac
Vasilis Raptakis <scaraveos@gmail.com>
2024-10-29 13:36:22 -0700

    Updated run scripts

--- scripts/runners/single-wf.sh
+++ scripts/runners/single-wf.sh
@@ -4,19 +4,24 @@
 
 set -e  # Exit immediately if a command fails
 
+# If the pipeline-zen root dir is provided, change directory to it
+if [ -n "$PZ_ROOT_DIR" ]; then
+  cd $PZ_ROOT_DIR
+fi
+
+# Set this to current directory if not set
+if [ -z "$PZ_ENV_DIR" ]; then
+  PZ_ENV_DIR=$(pwd)
+fi
+
 # Set the paths to the Python modules
-paths=$(pwd)/lib-common/src:$(pwd)/lib-workflows/$1/src
+paths=./lib-common/src
+paths=$paths:./lib-workflows/torchtunewrapper/src
 
 # Export .env environment variables
-eval $(cat ./.env | grep -v '^#' | tr -d '\r')
+set -o allexport
+eval $(cat $PZ_ENV_DIR/.env | grep -v '^#' | tr -d '\r')
 echo "PZ_ENV set to $PZ_ENV"
 
-# Export the variables so they're available to the python script
-export PZ_ENV
-export PZ_HUGGINGFACE_TOKEN
-export PZ_CUSTOMER_API_KEY
-export PZ_USE_MPS
-export PZ_RESULTS_BUCKET_SUFFIX
-
-# Run the script for the specified workflow
+# Run the Celery pipeline for the specified workflow
 PYTHONPATH=$paths python lib-workflows/$1/src/$1/cli.py "${@:2}"
