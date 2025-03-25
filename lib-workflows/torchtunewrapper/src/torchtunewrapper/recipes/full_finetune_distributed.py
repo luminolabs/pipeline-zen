4f4a338ac9ce1024f8cf39549ab9be11b55d89b7
srujan <srujan1997@gmail.com>
2025-03-25 03:07:49 +0000

    checkpoint on every epoch

--- lib-workflows/torchtunewrapper/src/torchtunewrapper/recipes/full_finetune_distributed.py
+++ lib-workflows/torchtunewrapper/src/torchtunewrapper/recipes/full_finetune_distributed.py
@@ -951,6 +951,7 @@ class FullFinetuneRecipeDistributed(RecipeBase):
                                       epoch_len=self.total_epochs,
                                       epoch_time_elapsed_s=time_per_epoch)
             t_epoch_start = time.perf_counter()
+            self.save_checkpoint(epoch=curr_epoch) # Checkpoint on every epoch
 
         self._profiler.stop()
 
